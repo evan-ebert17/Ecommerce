fc51a046140456319401d95e256b5071c8eb77f8
EvanEbert <evan.ebert17@gmail.com>
2021-08-23 13:14:53 -0500

    filled out models, routed products and linked server to sequelize

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  //foreignKey: 'product_id',
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'placeholder'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  //foreignKey: 'product_id',
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'description'
+});
 
 module.exports = {
   Product,
